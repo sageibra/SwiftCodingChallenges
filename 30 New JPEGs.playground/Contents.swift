7a3fce1342933b5df38f4448688625bad630afc8
sageibra <akaev_ibragim@mail.ru>
2021-03-08 18:55:29 +0300

    Added the solution for the thirtieth challenge

---
+++ 30 New JPEGs.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+/*
+ Write a function that accepts a path to a directory and returns an array of all JPEGs that have
+ been created in the last 48 hours.
+*/
+
+// Solution
+
+func getJPEGs(in directory: String) -> [String] {
+    let fm = FileManager.default
+    let directoryURL = URL(fileURLWithPath: directory)
+    guard let files = try? fm.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil) else { return [] }
+    
+    var jpegs = [String]()
+    for file in files {
+        if file.pathExtension == "jpg" || file.pathExtension == "jpeg" {
+            guard let attributes = try? fm.attributesOfItem(atPath: file.path) else { continue }
+            guard let creationDate = attributes[.creationDate] as? Date else { continue }
+            if creationDate > Date(timeIntervalSinceNow: -60 * 60 * 48) {
+                jpegs.append(file.lastPathComponent)
+            }
+        }
+    }
+    return jpegs
+}
